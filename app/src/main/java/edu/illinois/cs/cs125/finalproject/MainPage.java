614501be81132b5d0bf2b723f9d40edc6442a6db
ljm2 <ljm2@illinois.edu>
2018-04-23 10:26:06 -0500

    Basic UI

---
+++ app/src/main/java/edu/illinois/cs/cs125/finalproject/MainPage.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.cs125.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
